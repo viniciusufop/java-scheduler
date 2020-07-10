f70edede2beb4fa87eee95669358ce6eff9baa84
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-07-10 10:06:57 -0300

    integrando diretamente com graylog

--- src/main/java/br/com/vfs/javascheduler/scheduler/SchedulingTask.java
+++ src/main/java/br/com/vfs/javascheduler/scheduler/SchedulingTask.java
@@ -6,15 +6,25 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @Slf4j
 @Component
 @EnableScheduling
 public class SchedulingTask {
 
-    private static final long SECOND = 1000;
+    private static final long SECOND = 10000;
     @Scheduled(fixedDelay = SECOND)
     public void scheduleFutureTask() {
+
         log.info("Schedule execution in: {}", LocalDateTime.now());
+
+        Random random = new Random();
+        int teste = random.nextInt(5);
+
+        if(teste == 1) {
+            throw new RuntimeException("Teste exception randomica");
+        }
+
     }
 }
